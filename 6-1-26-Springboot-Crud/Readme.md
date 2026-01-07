3215efd060782451e65ad746da554aab9ca1577d
Suraj Honmane <surajhonmane18@gmail.com>
2026-01-07 16:14:34 +0530

    Add Spring Boot CRUD project as normal folder

---
+++ 6-1-26-Springboot-Crud/Readme.md
@@ -0,0 +1,25 @@
+# Student CRUD Application (Spring Boot + MongoDB)
+
+This is a simple **CRUD (Create, Read, Update, Delete)** application built using **Spring Boot** and **MongoDB**.  
+It exposes REST APIs to manage student data and can be tested using **Postman**.
+
+---
+
+## 🚀 Technologies Used
+- Java 21  
+- Spring Boot  
+- Maven  
+- MongoDB  
+- Postman (for API testing)
+
+---
+
+## 🔐 Environment Variables
+
+Create a `.env` file in the project root and add:
+MONGODB_URI=your_mongodb_connection_string
+
+---
+
+## ▶️ How to Run the Project
+.\mvnw spring-boot:run
---
+++ 6-1-26-Springboot-Crud/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Student;
+import com.example.demo.service.StudentService;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> addStudent(@RequestBody Student student) {
+        Student savedStudent = service.save(student);
+        return ResponseEntity.ok(savedStudent);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getAllStudents() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable String id) {
+        Student student = service.findById(id);
+        if (student == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(student);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(
+            @PathVariable String id,
+            @RequestBody Student student) {
+
+        student.setId(id);
+        Student updatedStudent = service.save(student);
+        return ResponseEntity.ok(updatedStudent);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.ok("Student deleted successfully");
+    }
+}
